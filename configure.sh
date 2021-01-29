091211f185d4e97c1b7648b5ce70c43d143dd71b
XDicks <78192717+XDicks@users.noreply.github.com>
2021-01-29 13:05:45 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -16,7 +16,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
 {
     "inbounds": [
         {
-            "port": 233,
+            "port": $port,
             "protocol": "vmess",
             "settings": {
                 "clients": [
